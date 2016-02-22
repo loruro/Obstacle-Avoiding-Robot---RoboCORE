e7a71a7146468b40193d42719e1eb0380bccc93f
loruro <loruro1@gmail.com>
2016-02-22 22:11:31 +0100

    Refactored

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,15 @@
  *  @brief Obstacle Avoiding Robot
  *  @author Karol Leszczyński
  */ 
- 
-#include <hFramework.h>
+
 #include <stdio.h>
+
+#include <hFramework.h>
 #include "Lego_Ultrasonic.h"
 #include "Lego_Touch.h"
+
 #include "sonar.h"
+
 using namespace hFramework;
 using namespace hSensors;
 
@@ -16,49 +19,45 @@ Sonar *sonar;
 #if REMOTE_CONTROL == 1
 #include "hCloudClient.h"
 
-void configHandler()
-{
-	sonar->configHandler();
+void configHandler() {
+  sonar->configHandler();
 }
 
-void onKeyEvent(KeyEventType type, int code)
-{
-	sonar->onKeyEvent(type, code);
+void onKeyEvent(KeyEventType type, int code) {
+  sonar->onKeyEvent(type, code);
 }
 
-void onButtonEvent(hId id, ButtonEventType type)
-{
-	sonar->onButtonEvent(id, type);
+void onButtonEvent(hId id, ButtonEventType type) {
+  sonar->onButtonEvent(id, type);
 }
 #endif
 
 /**
 * @brief Main.
 */
-void hMain(void)
-{
-	sys.setLogDev(&Serial);
+void hMain(void) {
+  sys.setLogDev(&Serial);
 
-	hMotor *mLeft = &hMot3;
-	hMotor *mRight = &hMot4;
-	hMotor *mScanner = &hMot1;
-	Lego_Ultrasonic sensorUltrasonic(hSens1);
-	Lego_Touch sensorLeft(hSens3);
-	Lego_Touch sensorRight(hSens2);
+  hMotor *mLeft = &hMot3;
+  hMotor *mRight = &hMot4;
+  hMotor *mScanner = &hMot1;
+  Lego_Ultrasonic sensorUltrasonic(hSens1);
+  Lego_Touch sensorLeft(hSens3);
+  Lego_Touch sensorRight(hSens2);
 
-	sonar = new Sonar(mLeft, mRight, mScanner,
-	&sensorUltrasonic, &sensorLeft, &sensorRight);
+  sonar = new Sonar(mLeft, mRight, mScanner, &sensorUltrasonic, &sensorLeft,
+      &sensorRight);
 
-	#if REMOTE_CONTROL == 1
-	platform.begin(&Usb);
-	platform.ui.configHandler = configHandler;
-	platform.ui.onKeyEvent = onKeyEvent;
-	platform.ui.onButtonEvent = onButtonEvent;
-	#endif
+#if REMOTE_CONTROL == 1
+  platform.begin(&Usb);
+  platform.ui.configHandler = configHandler;
+  platform.ui.onKeyEvent = onKeyEvent;
+  platform.ui.onButtonEvent = onButtonEvent;
+#endif
 
-	sonar->start();
+  sonar->start();
 
-	while(true){
-		sys.delay(20);
-	}
+  while (true) {
+    sys.delay(20);
+  }
 }
--- src/sonar.h
+++ src/sonar.h
@@ -2,11 +2,12 @@
  *  @brief Sonar header
  *  @author Karol Leszczyński
  */ 
- 
+
 #define REMOTE_CONTROL 1
 
-#include <hFramework.h>
 #include <stdio.h>
+
+#include <hFramework.h>
 #include "Lego_Ultrasonic.h"
 #include "Lego_Touch.h"
 
@@ -17,54 +18,100 @@
 using namespace hFramework;
 using namespace hSensors;
 
-class Sonar
-{
-public:
-	Sonar(hMotor *mLeft, hMotor *mRight, hMotor *mScanner,
-	Lego_Ultrasonic *sUltrasonic, Lego_Touch *sLeft, Lego_Touch *sRight);
-	~Sonar();
-	void start();
+class Sonar {
+ public:
+  /**
+  * @brief Sonar class constructor.
+  * @param hMotor object for left motor.
+  * @param hMotor object for right motor.
+  * @param hMotor object for scanner motor.
+  * @param Lego_Ultrasonic object for scanner sensor.
+  * @param Lego_Touch object for left touch sensor.
+  * @param Lego_Touch object for right touch sensor.
+  */
+  Sonar(hMotor *mLeft, hMotor *mRight, hMotor *mScanner,
+    Lego_Ultrasonic *sUltrasonic, Lego_Touch *sLeft, Lego_Touch *sRight);
 
-	#if REMOTE_CONTROL == 1
-	void configHandler();
-	void onKeyEvent(KeyEventType type, int code);
-	void onButtonEvent(hId id, ButtonEventType type);
-	#endif
+  /**
+  * @brief Sonar class destructor.
+  */
+  ~Sonar() {};
+  
+  /**
+  * @brief Calibrates scanner and then starts rest of the robot.
+  */
+  void start();
+#if REMOTE_CONTROL == 1
+  /**
+  * @brief Interface configuration.
+  */
+  void configHandler();
+  
+  /**
+  * @brief Key event.
+  */
+  void onKeyEvent(KeyEventType type, int code);
+  
+  /**
+  * @brief Button event.
+  */
+  void onButtonEvent(hId id, ButtonEventType type);
+#endif
 
-private:
-	const uint16_t calibrationStep = 930;
-	const uint16_t motorPower = 600;
-	const uint16_t scannerStep = 180;
-	const uint8_t criticalDistance = 20;
-	const uint8_t safeDistance = 35;
-	const uint16_t withdrawTime = 3000;
-	const uint16_t collisionWithdrawTime = 3000;
-	const uint16_t rotationTime = 1000;
+ private:
+  const uint16_t calibrationStep = 930;
+  const uint16_t motorPower = 600;
+  const uint16_t scannerStep = 180;
+  const uint8_t criticalDistance = 20;
+  const uint8_t safeDistance = 35;
+  const uint16_t withdrawTime = 3000;
+  const uint16_t collisionWithdrawTime = 3000;
+  const uint16_t rotationTime = 1000;
 
-	hMutex motor_mutex;
-	hMotor *motorLeft;
-	hMotor *motorRight;
-	hMotor *motorScanner;
-	Lego_Ultrasonic *sensorUltrasonic;
-	Lego_Touch *sensorLeft;
-	Lego_Touch *sensorRight;
+  /**
+  * @brief Gets distane to objects. Measurement is performed 5 times
+  *        and mean value is calculated.
+  * @return Distance to object.
+  */
+  float getDist();
+  
+  /**
+  * @brief Gets 7 distances to objects around robot.
+  * @param Arrat of size 7.
+  */
+  void getDistances(float *allDistances);
+  
+  /**
+  * @brief Calibrates scanner by setting it in desired zero position.
+  */
+  void calibrateScanner();
 
-	float getDist();
-	void getDistances(float *allDistances);
+  /**
+  * @brief Gets distances to objects around robot
+  *        and then moves towards safest direction.
+  */
+  void scanAndGo();
+  static void scanAndGoWrapper(void *parm) {
+    (static_cast<Sonar*>(parm))->scanAndGo();
+  }
 
-	void scanAndGo();
-	static void scanAndGoWrapper(void *parm){
-		(static_cast<Sonar*>(parm))->scanAndGo();
-	}
+  /**
+  * @brief Detects collision by using touch sensor
+  *        and then moves robot backwards for defined time.
+  */
+  void collisionProcedure();
+  static void collisionProcedureWrapper(void *parm) {
+    (static_cast<Sonar*>(parm))->collisionProcedure();
+  }
 
-	void collisionProcedure();
-	static void collisionProcedureWrapper(void *parm){
-		(static_cast<Sonar*>(parm))->collisionProcedure();
-	}
-
-	void calibrateScanner();
-
-	#if REMOTE_CONTROL == 1
-	bool remoteOn = false;
-	#endif
+  hMutex motor_mutex;
+  hMotor *motorLeft;
+  hMotor *motorRight;
+  hMotor *motorScanner;
+  Lego_Ultrasonic *sensorUltrasonic;
+  Lego_Touch *sensorLeft;
+  Lego_Touch *sensorRight;
+#if REMOTE_CONTROL == 1
+  bool remoteOn = false;
+#endif
 };
